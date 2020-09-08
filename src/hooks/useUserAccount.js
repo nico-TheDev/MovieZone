786b3d2d5b6e022f0cb41113ee5b29ffca3eba96
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-09-08 17:51:23 +0800

    fix 401 error with swr hooks

--- src/hooks/useUserAccount.js
+++ src/hooks/useUserAccount.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import useSWR from "swr";
 import API from "../api/moviedb.instance";
 
@@ -8,76 +7,17 @@ export default function useUserAccount(session) {
             session_id: session?.session_id,
         },
     };
-    const mediaConfig = {
-        params: {
-            session_id: session?.session_id,
-            sort_by: "created_at.desc",
-        },
-    };
-
     const detailsFetcher = () =>
         API.get("account", config).then((res) => res.data);
 
-    const { data: userData } = useSWR(
-        () => (session.session_id ? `api/account/` : null),
+    const { data, error } = useSWR(
+        session ? `api/account/` : null,
         detailsFetcher
     );
 
-    const getFavoriteMovies = API.get(
-            `account/${userData?.id}/favorite/movies`,
-            mediaConfig
-        ),
-        getFavoriteTV = API.get(
-            `account/${userData?.id}/favorite/tv`,
-            mediaConfig
-        ),
-        getRatedMovies = API.get(
-            `account/${userData?.id}/rated/movies`,
-            mediaConfig
-        ),
-        getRatedTV = API.get(`account/${userData?.id}/rated/tv`, mediaConfig),
-        getWatchListMovie = API.get(
-            `account/${userData?.id}/watchlist/movies`,
-            mediaConfig
-        ),
-        getWatchListTV = API.get(
-            `account/${userData?.id}/watchlist/tv`,
-            mediaConfig
-        );
-
-    const requests = [
-        getFavoriteMovies,
-        getFavoriteTV,
-        getRatedMovies,
-        getRatedTV,
-        getWatchListMovie,
-        getWatchListTV,
-    ];
-
-    const mediaFetcher = () =>
-        axios.all(requests).then(
-            axios.spread((...responses) => ({
-                favoriteMovies: responses[0].data,
-                favoriteTV: responses[1].data,
-                ratedMovies: responses[2].data,
-                ratedTV: responses[3].data,
-                watchListMovies: responses[4].data,
-                watchListTV: responses[5].data,
-            }))
-        );
-
-    const { data: userMedia, error, mutate } = useSWR(
-        () => (userData ? `api/user/media` : null),
-        mediaFetcher
-    );
-
     return {
-        isLoading: !userData && !userMedia,
-        data: {
-            user: userData,
-            media: userMedia,
-        },
+        isLoading: !data && !error,
+        data,
         hasError: error,
-        mutate,
     };
 }
