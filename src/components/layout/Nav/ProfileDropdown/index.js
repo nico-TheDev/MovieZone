d6046cd2636546cd293203f38c815984b5336f9d
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-09-09 14:18:02 +0800

    remove dropdown on nav

--- src/components/layout/Nav/ProfileDropdown/index.js
+++ src/components/layout/Nav/ProfileDropdown/index.js
@@ -1,33 +1,19 @@
-import React,{ useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 import getIcon from "util/getIcon";
-import {
-    DropdownHead,
-    DropdownItem,
-    DropdownButton,
-    DropdownBody,
-    UserIcon,
-} from "./styles";
+import { DropdownHead, DropdownButton, UserIcon } from "./styles";
 
 export default function ProfileDropdown() {
-    const [isOpen,setIsOpen] = useState(false);
-
-    const handleClick = () => setIsOpen(true);
-    const closePanel = () => setIsOpen(false);
-
     return (
-        <DropdownHead onMouseLeave={closePanel}>
-            <DropdownButton onClick={handleClick}>
-                <UserIcon>
-                    <use href={getIcon("user-circle")} />
-                </UserIcon>
-            </DropdownButton>
-            <DropdownBody isOpen={isOpen}>
-                <DropdownItem>
-                    <NavLink to="/login">Login</NavLink>
-                </DropdownItem>
-            </DropdownBody>
+        <DropdownHead>
+            <NavLink to="/login">
+                <DropdownButton as="div">
+                    <UserIcon>
+                        <use href={getIcon("user-circle")} />
+                    </UserIcon>
+                </DropdownButton>
+            </NavLink>
         </DropdownHead>
     );
 }
